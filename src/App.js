bc33eba2cc6cbb3089f040a742777341df5d78a2
Joseph Odulesi <Joe@Josephs-MacBook-Pro.local>
2023-04-05 14:32:42 +0100

    Development code for the static website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Top from "./Components/Top";
+
+function App() {
+  return (
+    <div className="app">
+      <Top />
+    </div>
+  );
+}
+
+export default App;
